99e6b6b2e9599265326a106482626f1bf619b683
Philipp Heuer <git@philippheuer.me>
2024-07-14 02:10:40 +0200

    fix: add isAlphaNumeric util function

---
+++ pkg/core/util/validate.go
@@ -0,0 +1,14 @@
+package util
+
+import (
+	"unicode"
+)
+
+func IsAlphanumeric(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) && !unicode.IsDigit(r) {
+			return false
+		}
+	}
+	return true
+}
